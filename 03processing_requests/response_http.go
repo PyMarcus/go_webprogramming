aa418408b3ba6dc074756b95e8c54810e1cd62c7
PyMarcus <marcus-v@outlook.com>
2023-07-29 14:46:23 -0300

    http form cookie and flash message

--- 03processing_requests/response_http.go
+++ 03processing_requests/response_http.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 )
 
 func root(w http.ResponseWriter, r *http.Request){
@@ -44,6 +47,50 @@ func jsonResponse(w http.ResponseWriter, r *http.Request){
 	w.Write(j)
 }
 
+func cookie(w http.ResponseWriter, r *http.Request){
+	c1 := &http.Cookie{
+		Name: "fcookie",
+		Value: "322",
+		HttpOnly: true,
+	}
+
+	c2 := &http.Cookie{
+		Name: "2cookie",
+		Value: "3223",
+		HttpOnly: true,
+		Secure: true,
+
+	}
+
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
+
+	fmt.Println(r.Header["Cookie"])
+	fmt.Println(r.Cookies())
+}
+
+//flash messages in the cookies
+func setMessage(w http.ResponseWriter, r *http.Request){
+	c := http.Cookie{
+		Name: "flash",
+		Value: base64.URLEncoding.EncodeToString([]byte("Hello, world!")),
+	}
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request){
+	c, _ := r.Cookie("flash")
+	val, _ := base64.URLEncoding.DecodeString(c.Value)
+	rc := http.Cookie{
+		Name: "flash",
+		MaxAge: -1, //delete cookie
+		Expires: time.Unix(1, 0),
+	}
+
+	http.SetCookie(w, &rc)
+	fmt.Fprint(w, string(val))
+}
+
 
 func main(){
 	server := http.Server{
@@ -53,5 +100,8 @@ func main(){
 	http.HandleFunc("/error", wExample)
 	http.HandleFunc("/redirect", redirect)
 	http.HandleFunc("/json", jsonResponse)
+	http.HandleFunc("/cookie", cookie)
+	http.HandleFunc("/flash", showMessage)
+	http.HandleFunc("/set_flash", setMessage)
 	server.ListenAndServe()
 }
